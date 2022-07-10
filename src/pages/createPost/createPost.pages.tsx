a03668b79d4f3d5ab8f9610583b97499cefbb689
Егор <egornesvickij@gmail.com>
2022-07-10 11:57:18 +0500

    Created form.

--- src/pages/createPost/createPost.pages.tsx
+++ src/pages/createPost/createPost.pages.tsx
@@ -1,8 +1,79 @@
 import React from "react";
 import "./createPost.pages.scss";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
+const currencies = [
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'BTC',
+    label: '฿',
+  },
+  {
+    value: 'JPY',
+    label: '¥',
+  },
+];
 
 export const CreatePost = (): JSX.Element => {
+
+  const [currency, setCurrency] = React.useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrency(event.target.value);
+  };
+
   return (
-    <div>Create Post</div>
+    <Box
+      className="createPostForm"
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        id="outlined-multiline-static"
+        label="Запись"
+        multiline
+        rows={4}
+        className="createPostForm_textInput"
+      />
+      <TextField
+        id="outlined-basic"
+        className="createPostForm_autorInput"
+        required
+        label="Подпись"
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-select-currency"
+        className="createPostForm_timeSelect"
+        select
+        label="Точное время по:"
+        value={currency}
+        onChange={handleChange}
+      >
+        {currencies.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Создать
+      </Button>
+    </Box>
   )
 }
